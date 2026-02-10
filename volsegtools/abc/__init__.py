13fb810fd720feda4b0a46db9be9826c60cf65c1
Daniel Kříž <daniel.kriz@protonmail.com>
2026-02-10 17:40:45 +0100

    fix: circular module imports

--- volsegtools/abc/__init__.py
+++ volsegtools/abc/__init__.py
@@ -1,5 +1,5 @@
-from .converter import Converter
 from .data_handle import DataHandle
+from .converter import Converter
 from .downsampler import Downsampler
 from .kernel import ConvolutionKernel
 from .preprocessor import Preprocessor
--- volsegtools/core/__init__.py
+++ volsegtools/core/__init__.py
@@ -1,5 +1,5 @@
-from .bounds import Bounds
-from .downsampling_parameters import DownsamplingParameters, to_bytes
-from .gaussian_kernel_3D import Gaussian3DKernel
 from .lattice_kind import LatticeKind
 from .vector import Vector3
+from .bounds import Bounds
+from .gaussian_kernel_3D import Gaussian3DKernel
+from .downsampling_parameters import DownsamplingParameters, to_bytes
