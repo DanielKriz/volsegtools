717ece9476a4203aae6a6523a8786345f06ff2e6
Daniel Kříž <daniel.kriz@protonmail.com>
2026-05-17 00:48:40 +0200

    chore: remove unused old serializer interface

--- src/volsegtools/abc/serializer.py
+++ src/volsegtools/abc/serializer.py
@@ -12,10 +12,3 @@ class Serializer(abc.ABC):
     @abc.abstractmethod
     async def serialize(data_set, output_path: Path) -> List[Path]: ...
 
-
-class SerializerOld(abc.ABC):
-    """Serializes the provided array-like data into some data format."""
-
-    @staticmethod
-    @abc.abstractmethod
-    async def serialize(data: DataHandle, output_path: Path) -> List[Path]: ...
