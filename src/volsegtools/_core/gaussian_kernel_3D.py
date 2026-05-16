186a2185cdd1abe7bd9ee9ff6fe091d363e970ba
Daniel Kříž <daniel.kriz@protonmail.com>
2026-05-16 19:39:03 +0200

    feat: add size property to gaussian kernel

--- src/volsegtools/_core/gaussian_kernel_3D.py
+++ src/volsegtools/_core/gaussian_kernel_3D.py
@@ -7,10 +7,15 @@ class Gaussian3DKernel(ConvolutionKernel):
     """Generate a 3D Gaussian kernel."""
 
     def __init__(self, size: int, sigma: float):
+        self._size = size
         ax = np.linspace(-(size // 2), size // 2, size)
         xx, yy, zz = np.meshgrid(ax, ax, ax, indexing="ij")
         kernel = np.exp(-(xx**2 + yy**2 + zz**2) / (2.0 * sigma**2))
         self._kernel_lattice = kernel / np.sum(kernel)
+
+    @property
+    def size(self) -> int:
+        return self._size
 
     def as_ndarray(self) -> np.ndarray:
         return self._kernel_lattice
