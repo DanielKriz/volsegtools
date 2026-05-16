ada19e61bd5372d9dc8aaa2e67ffb9ff6892dac6
Daniel Kříž <daniel.kriz@protonmail.com>
2026-05-16 19:34:34 +0200

    feat: properly document the bundler interface

--- src/volsegtools/abc/downsampling_strategy.py
+++ src/volsegtools/abc/downsampling_strategy.py
@@ -1,5 +1,30 @@
 import abc
-
+from typing import Any
 
 class DownsamplingStrategy(abc.ABC):
-    def execute(self, channel): ...
+    """Downsamples given data."""
+    MIN_SIZE_THRESHOLD = 5_000_000  # 5 MB
+    def execute(self, data) -> Any:
+        """Executes give downsampling strategy on given data.
+
+        It shall be implemented as a generator, where each yield represents
+        one smaller resolution of the given data.
+
+        Parameters
+        ----------
+        data: Union[trimesh.Trimesh, Channel]
+            Data that shall be downsampled by the algorithm implemented by
+            the strategy. Some strategies might work with meshes and not
+            channels.
+
+        Raises
+        ------
+        TypeError
+            If the given data is not of supported type.
+
+        Yields
+        ------
+        Union[trimesh.Trimesh, Channel]
+            Downsampled version of the data.
+        """
+        ...
