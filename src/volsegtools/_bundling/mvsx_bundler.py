7bca0e6acddb5dc8c06902510441082e0fa6be9c
Daniel Kříž <daniel.kriz@protonmail.com>
2026-05-16 19:39:59 +0200

    feat: add colors to the mvsx bundling

--- src/volsegtools/_bundling/mvsx_bundler.py
+++ src/volsegtools/_bundling/mvsx_bundler.py
@@ -10,8 +10,32 @@ from molviewspec.mvsx_converter import tempfile
 from volsegtools._model.data_set import info_from_file_path
 from volsegtools.abc.bundler import Bundler
 
+vst_logger = logging.getLogger("volsegtools")
+
+# It is better to distinguish between the individual primitives with colors.
+X11_COLOR_NAMES = [
+    "silver",
+    "red",
+    "aqua",
+    "green",
+    "wheat",
+    "orange",
+    "pale green",
+    "violet",
+    "sienna",
+    "olive",
+    "pink",
+    "ivory",
+    "purple",
+    "coral",
+    "blue",
+    "yellow",
+    "brown",
+]
 
 class MVSXBundler(Bundler):
+    """Bundler for the MVSX format."""
+
     def bundle(self, data_paths: List[Path], output_path: Path) -> List[Path]:
         parsed_paths = [info_from_file_path(x) for x in data_paths]
 
@@ -25,12 +49,12 @@ class MVSXBundler(Bundler):
             builder = mvs.create_builder()
 
             data_set_id = data_per_resolution[resolution][0].data_set
-            logging.info(
-                f"Bundling Data set {data_set_id} with resolution {resolution}"
+            vst_logger.info(
+                f"Bundling Data set '{data_set_id}' with resolution {resolution}"
             )
 
-            for info in data_per_resolution[resolution]:
-                logging.info(f"... Adding {info.file_path.name}")
+            for idx, info in enumerate(data_per_resolution[resolution]):
+                vst_logger.info(f"... Adding {info.file_path.name}")
                 volume = (
                     builder.download(url=str(info.file_path))
                     .parse(format="map")
@@ -38,8 +62,14 @@ class MVSXBundler(Bundler):
                 )
                 volume.representation(
                     type="isosurface", relative_isovalue=2, show_wireframe=False
-                ).color(color="red")
+                ).color(color=X11_COLOR_NAMES[idx % len(X11_COLOR_NAMES)])
 
+            archive_path = output_path / Path(
+                f"{data_set_id}_r{resolution}.mvsx"
+            )
+
+            # At the moment we have to create a temporary file to use the mvs
+            # API for creation of MVSX
             with tempfile.NamedTemporaryFile(mode="w", delete=True) as tmp_file:
                 state = mvs.MVSJ(data=builder.get_state()).dumps()
                 tmp_file.write(state)
@@ -49,9 +79,10 @@ class MVSXBundler(Bundler):
                 output_file_paths.append(
                     mvs.mvsj_to_mvsx(
                         tmp_file.name,
-                        output_path / Path(f"{data_set_id}_r{resolution}.mvsx"),
+                        archive_path,
                         download_external=True,
                     )
                 )
+                vst_logger.info(f"Created MVSX archive at: {archive_path}")
 
         return output_file_paths
