2f5a10c362b4c707bb2c60adbd7bef2c2554a056
Daniel Kříž <daniel.kriz@protonmail.com>
2026-05-16 19:43:50 +0200

    fix: add missing documentation to the bundler interface

--- src/volsegtools/abc/bundler.py
+++ src/volsegtools/abc/bundler.py
@@ -4,7 +4,26 @@ from pathlib import Path
 
 
 class Bundler(abc.ABC):
-    """Bundles a collection of data sets into a single artifact"""
+    """Bundles a collection of data sets into an another artifact.
+
+    For some formats and workflows it might be beneficial to return mutliple
+    artifacts, not just one.
+    """
 
     @abc.abstractmethod
-    def bundle(self, data_paths: List[Path], output_path: Path) -> List[Path]: ...
+    def bundle(self, data_paths: List[Path], output_path: Path) -> List[Path]:
+        """Bundles provided files into an another artifact.
+
+        Parameters
+        ----------
+        data_paths : List[Path]
+            List of files that should be bundled together.
+        output_path: Path
+            Path to which should the result be stored.
+
+        Returns
+        -------
+        List[Path]
+            List of resulting artifacts.
+        """
+        ...
