2dd995707a896fa7263e071adad13836d8262c97
Daniel Kříž <daniel.kriz@protonmail.com>
2026-08-26 14:21:49 +0200

    feat: add documentation and tests for unit kind

--- src/volsegtools/_core/unit_kind.py
+++ src/volsegtools/_core/unit_kind.py
@@ -1,15 +1,31 @@
 import enum
 
+# It would be better to use StrEnum for this case, but we would need
+# a multi-value enumeration for that. But that is better supported in 3.13+
+# which we currently cannot support.
+
 
 class UnitKind(enum.IntEnum):
-    """ """
+    """Representation of the measurement unit of the data."""
 
     UNKNOWN = 0
     MICRO_METER = 1
     ANGSTROM = 2
 
 
-def unit_from_str(string):
+def unit_from_str(string: str) -> UnitKind:
+    """Create a unit from a string.
+
+    Parameters
+    ----------
+    string: str
+        String representation of the unit name.
+
+    Returns
+    -------
+    UnitKind:
+        Enum representation of the string.
+    """
     match string:
         case "um" | "μm" | "micro":
             return UnitKind.MICRO_METER
@@ -19,13 +35,29 @@ def unit_from_str(string):
             return UnitKind.UNKNOWN
 
 
-def to_angstrom(micrometers):
-    return micrometers * 10000
+def to_angstrom(micrometers: float) -> float:
+    """Turns micrometers into angstroms.
+
+    Parameters
+    ----------
+    micrometers: float
+        Number of micrometers.
+
+    Returns
+    -------
+    float:
+        The same amount in angstroms.
+    """
+    return micrometers * 10_000
 
 
-def to_micrometer(angstroms):
-    return angstroms / 10000
+def to_micrometer(angstroms: float) -> float:
+    """Turns angstroms into micrometers.
 
+    Parameters
+    ----------
+    angstroms: float
+        Number of angstroms.
 
     Returns
     -------
---
+++ tests/core/test_unit_kind.py
@@ -0,0 +1,51 @@
+import volsegtools as vst
+import pytest
+
+
+@pytest.mark.parametrize(
+    "micrometers, angstroms",
+    [
+        (0, 0),
+        (1, 10_000),
+        (0.123, 1_230),
+    ],
+)
+def test_to_angstroms(micrometers, angstroms):
+    assert vst.to_angstrom(micrometers) == angstroms
+
+
+@pytest.mark.parametrize(
+    "micrometers, angstroms",
+    [
+        (0, 0),
+        (1, 10_000),
+        (0.123, 1_230),
+    ],
+)
+def test_to_micrometers(micrometers, angstroms):
+    assert vst.to_micrometer(angstroms) == micrometers
+
+
+@pytest.mark.parametrize("micrometers", [0, 1, 0.123])
+def test_to_angstroms_and_back(micrometers):
+    assert vst.to_micrometer(vst.to_angstrom(micrometers)) == micrometers
+
+
+@pytest.mark.parametrize("angstroms", [0, 10_000, 1_2300])
+def test_to_micrometers_and_back(angstroms):
+    assert vst.to_angstrom(vst.to_micrometer(angstroms)) == angstroms
+
+
+@pytest.mark.parametrize(
+    "string, kind",
+    [
+        ("um", vst.UnitKind.MICRO_METER),
+        ("μm", vst.UnitKind.MICRO_METER),
+        ("micro", vst.UnitKind.MICRO_METER),
+        ("angstrom", vst.UnitKind.ANGSTROM),
+        ("Å", vst.UnitKind.ANGSTROM),
+        ("wrong", vst.UnitKind.UNKNOWN),
+    ],
+)
+def test_unit_from_str(string, kind):
+    assert vst.unit_from_str(string) == kind
