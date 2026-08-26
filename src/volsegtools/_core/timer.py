b15569ce8f1ae13d485f16e65926a821e71a1bcb
Daniel Kříž <daniel.kriz@protonmail.com>
2026-08-26 14:23:15 +0200

    feat(Timer): add docs, tests and small fixes

--- src/volsegtools/_core/timer.py
+++ src/volsegtools/_core/timer.py
@@ -26,22 +26,65 @@ class TimerReporter(Protocol):
 
 
 class Timer:
+    """Timer for measuring processing time of stages and events.
+
+    Attributes
+    ----------
+    RESOLUTION: int
+        How many decimal numbers are considered.
+    start: float
+        Unix time denoting the start of measurement collection.
+    points: list[Timer.Stage | Timer.Event]
+        List of all measurements.
+    current_event: Timer.Event | None
+        Current event measurement, if there is any.
+    current_stage: Timer.Stage | None
+        Current stage measurement, if there is any.
+    """
+
     RESOLUTION = 3
 
     class Stage:
+        """Represents a single stage measurement in a pipeline.
+
+        Attributes
+        ----------
+        events: list[Timer.Event]
+            List of events that have happened during this stage.
+        start: float
+            Unix time denoting the start of this stage.
+        end: float
+            Unix time denoting the end of this stage.
+        name: str
+            Name of this stage.
+        """
+
         def __init__(self, name: str):
             self.events: list[Timer.Event] = []
-            self.start = time.time()
+            self.start: float = time.time()
             self.end: float | None = None
-            self.name = name
+            self.name: str = name
 
-        def push_event(self, name: str):
+        def push_event(self, name: str) -> None:
+            """Adds named event to this stage.
+
+            Parameters
+            ----------
+            name: str
+                Name of the event.
+            """
             if len(self.events) != 0:
                 self.events.append(Timer.Event(self.events[-1].end, name, self))
             else:
                 self.events.append(Timer.Event(self.start, name, self))
 
-        def pop_event(self):
+        def pop_event(self) -> None:
+            """Pops the current active event.
+
+            Basically marks the event a finished, if there is any.
+
+            If there is no current event, then it simply does nothing.
+            """
             if len(self.events) == 0:
                 return
             if self.events[-1].end is None:
@@ -49,11 +92,38 @@ class Timer:
 
         @property
         def total_time(self) -> float:
+            """Calculates the total time of the stage.
+
+            The stage has to be finished to calculate the total time.
+
+            Raises
+            ------
+            RuntimeError:
+                if the stage is not finished.
+
+            Returns
+            -------
+            float
+                The total time spent in the stage.
+            """
             if self.end is None:
                 raise RuntimeError("There was not measurement")
             return self.end - self.start
 
-        def serialize(self, use_ms=False):
+        def serialize(self, use_ms: bool = False) -> dict:
+            """Serializes the current stage into a dictionary.
+
+            Parameters
+            ----------
+            use_ms: bool
+                Flag that denotes whether the result should be saved in Unix
+                time or in miliseconds.
+
+            Returns
+            -------
+            dict:
+                Serialized stage.
+            """
             self.pop_event()
             return {
                 "type": "stage",
@@ -68,13 +138,48 @@ class Timer:
             }
 
     class Event:
+        """Represents an single event measurement of a stage or pipeline.
+
+        Key difference from the stage is that the event happens once, it is not
+        a continuous part of the timeline.
+
+        Therefore, it is immediatelly ended and for the calculation of total
+        time we need the start time of the parent stage/start of the timer
+        measurement.
+
+        Attributes
+        ----------
+        parent: Timer.Stage | None
+            Reference to the parent stage of this event. If there is none,
+            then the event has happened outside of any stage.
+        name: str
+            Name of the event.
+        start: float
+            Unix time denoting the start of the stage.
+        end: float
+            Unix time denoting the end of this event.
+        """
+
         def __init__(self, start: float, name: str, parent: Any | None = None):
             self.parent: Timer.Stage | None = parent
             self.name: str = name
             self.start: float = start
             self.end: float = time.time()
 
-        def serialize(self, use_ms=False):
+        def serialize(self, use_ms=False) -> dict:
+            """Serializes the current event into a dictionary.
+
+            Parameters
+            ----------
+            use_ms: bool
+                Flag that denotes whether the result should be saved in Unix
+                time or in miliseconds.
+
+            Returns
+            -------
+            dict:
+                Serialized event.
+            """
             return {
                 "type": "event",
                 "name": self.name,
@@ -88,6 +193,16 @@ class Timer:
 
         @property
         def total_time(self) -> float:
+            """Calculates the total time of the event.
+
+            In comparison of a stage, event is not continuous and is always
+            ended.
+
+            Returns
+            -------
+            float
+                The total time spent in the event.
+            """
             return self.end - self.start
 
     def __init__(self):
@@ -96,28 +211,54 @@ class Timer:
         self.current_event: Timer.Event | None = None
         self.current_stage: Timer.Stage | None = None
 
-    def restart(self):
+    def restart(self) -> None:
+        """Clears taken measurements and resets the start time point."""
         self.start = time.time()
         self.points = []
 
-    def push_stage(self, name: str):
+    def push_stage(self, name: str) -> None:
+        """Adds named stage measurement.
+
+        Parameters
+        ----------
+        name: str
+            Name of the stage.
+        """
         if self.current_stage is not None:
             self.pop_stage()
         self.current_stage = Timer.Stage(name)
         self.points.append(self.current_stage)
 
     def pop_stage(self):
+        """Pops the current active stage.
+
+        Marks the stage as finished. If there is none, then it does nothing.
+        """
         if self.current_stage is not None:
             self.current_stage.end = time.time()
             self.current_stage = None
 
     def pop_event(self):
+        """Pops the current active event.
+
+        Marks the event as finished. If there is none, then it does nothing.
+        """
         if self.current_stage is not None:
             self.current_stage.pop_event()
         elif len(self.points) != 0:
             self.points[-1].end = time.time()
 
     def push_event(self, name: str):
+        """Adds named event measurement.
+
+        If there is current stage present, then this operation is delegated.
+        Otherwise the timer is the owner of this measurement.
+
+        Parameters
+        ----------
+        name: str
+            Name of the event.
+        """
         if self.current_stage is not None:
             self.current_stage.push_event(name)
             self.current_event = self.current_stage.events[-1]
@@ -125,10 +266,31 @@ class Timer:
             self.current_event = Timer.Event(name)
             self.points.append(self.current_event)
 
-    def total_time(self):
-        return self.start + sum([p.end - p.start for p in self.points])
+    def total_time(self) -> float:
+        """Calculated the total time of the whole measurement.
 
-    def serialize(self, use_ms=False):
+        Raises
+        ------
+        RuntimeError:
+            if any of the measurement points is not ended.
+
+        Returns
+        -------
+        float:
+            The total time of the whole measurement.
+        """
+
+        def calculate_partial_time(start: float, end: float | None) -> float:
+            if end is None:
+                raise RuntimeError("There was not measurement")
+            return end - start
+
+        return self.start + sum(
+            [calculate_partial_time(p.start, p.end) for p in self.points]
+        )
+
+    def serialize(self, use_ms: bool = False) -> dict:
+        """Serializes the whole timer's measurement into a directory"""
         self.pop_stage()
         return {
             "total_time": round(self.total_time() - self.start, Timer.RESOLUTION)
@@ -137,7 +299,14 @@ class Timer:
             "stamps": [p.serialize(use_ms) for p in self.points],
         }
 
-    def print_report(self, reporter):
+    def print_report(self, reporter: TimerReporter) -> None:
+        """Prints the measurements using some reporter.
+
+        Parameters
+        ----------
+        reporter: TimerReporter
+            Instance of timer reporter to which is delegated the reporting.
+        """
         reporter.report(self.serialize(True))
 
 
@@ -147,11 +316,27 @@ class StandardReporter(TimerReporter):
     STAGE_FMT = "({:6.3f}%) Stage: '{}' ({:0.3f}s / {:0.3f}s / {:0.3f}s)"
     EVENT_FMT = "({:6.3f}% / {:6.3f}%) Event: '{}' ({:0.3f}s / {:0.3f}s / {:0.3f}s)"
 
-    def to_percent(self, part: float, total: float):
+    def _to_percent(self, part: float, total: float) -> float:
+        """Calculates the percentage.
+
+        Parameters
+        ----------
+        part: float
+            ...
+        total: float
+            ...
+
+        Returns
+        -------
+        float:
+            The percentage in the interval <0, 1.0>.
+        """
         step = total / 100.0
+        if math.isclose(step, 0):
+            return 0.0
         return part / step
 
-    def report(self, timer_data):
+    def report(self, timer_data) -> None:
         total_time = timer_data["total_time"]
 
         print("--------------------------------------------------------------")
@@ -163,14 +348,14 @@ class StandardReporter(TimerReporter):
         for stamp in timer_data["stamps"]:
             cummulative_total += stamp["time"]
             if stamp["type"] == "stage":
-                self.report_stage(stamp, timer_data["total_time"], cummulative_total)
+                self._report_stage(stamp, timer_data["total_time"], cummulative_total)
             else:
-                self.report_event(stamp, total_time, 100, cummulative_total, orhan=True)
+                self._report_event(stamp, total_time, 100, cummulative_total, orhan=True)
 
-    def report_stage(self, stage, total, cummulative_total):
+    def _report_stage(self, stage, total, cummulative_total):
         print(
             self.STAGE_FMT.format(
-                self.to_percent(stage["time"], total),
+                self._to_percent(stage["time"], total),
                 stage["name"],
                 stage["time"],
                 cummulative_total,
@@ -180,14 +365,14 @@ class StandardReporter(TimerReporter):
         cummulative_time = 0.0
         for event in stage["events"]:
             cummulative_time += event["time"]
-            self.report_event(event, total, stage["time"], cummulative_time)
+            self._report_event(event, total, stage["time"], cummulative_time)
 
-    def report_event(self, event, total, total_stage, cummulative, orhan=False):
+    def _report_event(self, event, total, total_stage, cummulative, orhan=False):
         fmt_str = self.EVENT_FMT if not orhan else "- " + self.EVENT_FMT
         print(
             fmt_str.format(
-                self.to_percent(event["time"], total),
-                self.to_percent(event["time"], total_stage),
+                self._to_percent(event["time"], total),
+                self._to_percent(event["time"], total_stage),
                 event["name"],
                 event["time"],
                 cummulative,
@@ -204,7 +389,7 @@ class JSONTimerReporter(TimerReporter):
         self.time = datetime.datetime.now()
         self.additional_columns = kwargs
 
-    def create_record(self, kind, text, total_time, stage=-1):
+    def _create_record(self, kind, text, total_time, stage=-1):
         record = {
             "label": self.label,
             "timestamp": f"{self.time}",
@@ -226,7 +411,7 @@ class JSONTimerReporter(TimerReporter):
         for stamp in timer_data["stamps"]:
             if stamp["type"] == "stage":
                 records.append(
-                    self.create_record(
+                    self._create_record(
                         kind="stage",
                         text=stamp["name"],
                         total_time=stamp["time"],
@@ -234,7 +419,7 @@ class JSONTimerReporter(TimerReporter):
                 )
                 for event in stamp["events"]:
                     records.append(
-                        self.create_record(
+                        self._create_record(
                             kind="event",
                             text=event["name"],
                             total_time=event["time"],
@@ -244,7 +429,7 @@ class JSONTimerReporter(TimerReporter):
                 stage_idx += 1
             else:
                 records.append(
-                    self.create_record(
+                    self._create_record(
                         kind="event",
                         text=stamp["name"],
                         total_time=stamp["time"],
---
+++ tests/core/test_timer.py
@@ -0,0 +1,62 @@
+import volsegtools as vst
+import json
+
+
+def test_standard_reporter(capsys):
+    timer = vst.Timer()
+    timer.push_stage("test_stage")
+    timer.push_event("test_event")
+    timer.pop_event()
+    timer.pop_stage()
+    timer.print_report(vst.StandardReporter())
+    out, _ = capsys.readouterr()
+
+    expected = (
+        "--------------------------------------------------------------\n"
+        "Time Report, total time = 0.0s\n"
+        "--------------------------------------------------------------\n"
+        "( 0.000%) Stage: 'test_stage' (0.000s / 0.000s / 0.000s)\n"
+        "( 0.000% /  0.000%) Event: 'test_event' (0.000s / 0.000s / 0.000s)\n"
+    )
+
+    assert out == expected
+
+
+def test_json_reporter(tmp_path):
+    timer = vst.Timer()
+    timer.push_stage("test_stage")
+    timer.push_event("test_event")
+    timer.pop_event()
+    timer.pop_stage()
+
+    output_file = tmp_path / "json_report.json"
+
+
+    timer.print_report(vst.JSONTimerReporter(
+        output_path=output_file,
+        label="input_file_name",
+        method="some_method",
+    ))
+
+    with open(output_file, "r") as file:
+        data = json.load(file)
+
+    assert data[0]["kind"] == "stage"
+    assert data[0]["label"] == "input_file_name"
+    assert data[0]["method"] == "some_method"
+    assert data[0]["text"] == "test_stage"
+    assert data[0]["total_time"] == 0.0
+
+    assert data[0].get("stage_id", None) != None
+    assert data[0].get("timer_id", None) != None
+    assert data[0].get("timestamp", None) != None
+
+    assert data[1]["kind"] == "event"
+    assert data[1]["label"] == "input_file_name"
+    assert data[1]["method"] == "some_method"
+    assert data[1]["text"] == "test_event"
+    assert data[1]["total_time"] == 0.0
+
+    assert data[1].get("stage_id", None) != None
+    assert data[1].get("timer_id", None) != None
+    assert data[1].get("timestamp", None) != None
