2c04d2bc17181b61768ae6abe113d9364f708a30
Daniel Kříž <daniel.kriz@protonmail.com>
2026-09-10 16:05:58 +0200

    chore: use better names in calculation of sizes

--- src/volsegtools/_downsampling/common.py
+++ src/volsegtools/_downsampling/common.py
@@ -1,48 +1,38 @@
-from volsegtools._storage.channel import Channel
-from volsegtools._core.bytes import Bytes
 import math
+
+from volsegtools._core.bytes import Bytes
+from volsegtools._storage.channel import Channel
 
 
 def calculate_approx_downsampled_sizes(
-    channel: Channel | int, size_threshold: int, factor: int = 3
+    channel: Channel | int, threshold: int, factor: int = 3
 ) -> list[float]:
-    if isinstance(channel, Channel):
-        bytes_count = channel.handle.nbytes
-    else:
-        bytes_count = channel
+    nbytes = channel.handle.nbytes if isinstance(channel, Channel) else channel
 
     sizes = []
-    while bytes_count > size_threshold:
-        bytes_count /= 2**factor
-        if bytes_count < size_threshold:
+    while nbytes > threshold:
+        nbytes /= 2**factor
+        if nbytes < threshold:
             break
-        sizes.append(bytes_count)
+        sizes.append(nbytes)
     return sizes
 
 
-def calculate_steps(channel: Channel | int, size_threhold: int, factor: int = 2) -> int:
-    if isinstance(channel, Channel):
-        bytes_count = channel.handle.nbytes
-    else:
-        bytes_count = channel
+def calculate_steps(channel: Channel | int, threhold: int, factor: int = 2) -> int:
+    nbytes = channel.handle.nbytes if isinstance(channel, Channel) else channel
+    return len(calculate_approx_downsampled_sizes(nbytes, threhold, factor))
 
-    return len(calculate_approx_downsampled_sizes(bytes_count, size_threhold, factor))
 
 def calculate_min_size_from_resolutions(
-    channel: Channel | int,
-    resolution_cnt: int,
-    factor: int = 2
+    channel: Channel | int, resolution_cnt: int, factor: int = 2
 ) -> Bytes:
-    if isinstance(channel, Channel):
-        bytes_count = channel.handle.nbytes
-    else:
-        bytes_count = channel
+    nbytes = channel.handle.nbytes if isinstance(channel, Channel) else channel
 
-    min_size = bytes_count
+    min_size = nbytes
     for _ in range(resolution_cnt):
-        bytes_count /= 2**factor
-        bytes_count = int(bytes_count)
-        if math.isclose(bytes_count, 0):
+        nbytes /= 2**factor
+        nbytes = int(nbytes)
+        if math.isclose(nbytes, 0):
             break
-        min_size = bytes_count
+        min_size = nbytes
     return Bytes(min_size)
